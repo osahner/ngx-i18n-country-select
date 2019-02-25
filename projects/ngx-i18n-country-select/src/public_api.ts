509ca74ad8f7690cdd1cada8030b7576a5e38daf
Oliver Sahner <osahner@medianet.de>
2019-02-25 11:23:39 +0100

    fix: code style

--- projects/ngx-i18n-country-select/src/public_api.ts
+++ projects/ngx-i18n-country-select/src/public_api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-i18n-country-select
  */
 
-export * from './lib/ngx-i18n-country-select.service';
-export * from './lib/ngx-i18n-country-select.component';
-export * from './lib/ngx-i18n-country-select.module';
+export * from './lib/i18n-country-select.service';
+export * from './lib/i18n-country-select.component';
+export * from './lib/i18n-country-select.module';
